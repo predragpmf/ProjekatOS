788c4d3b23f67c84ed914901d2c15c7581f43f15
Предраг Лазаревић <83598542+predragpmf@users.noreply.github.com>
2022-04-10 14:35:35 +0200

    Pakovanje

--- src/start/Main.java
+++ src/start/Main.java
@@ -1,9 +1,24 @@
 package start;
 
+/*
+import java.io.*;
+import java.awt.GraphicsEnvironment;
+import java.net.URISyntaxException;
+*/
+
 public class Main {
 
-    public static void main(String[] args){
-	    Shell.start();
+    public static void main(String[] args) /*throws IOException, InterruptedException, URISyntaxException*/ {
+        /*Console console = System.console();
+        if(console == null && !GraphicsEnvironment.isHeadless()){
+            String filename = Main.class.getProtectionDomain().getCodeSource().getLocation().toString().substring(6);
+            Runtime.getRuntime().exec(new String[]{"cmd","/c","start","cmd","/k","java -jar \"" + filename + "\""});
+        }else{
+            Shell.start();
+            System.out.println("Program has ended, please type 'exit' to close the console");
+        }
+        */
+        Shell.start();
     }
 
 }
